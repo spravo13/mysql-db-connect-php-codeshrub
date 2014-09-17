f6e3016ddb5faad5c3e4b8e4b8bec343674a51e2
Spencer Bravo <spencer@techandcode.com>
2014-09-17 16:43:41 -0700

    Initial commit.

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+$db_host = '  ';      // location of your database(localhost ussualy works)
+$db_user = '  ';      // your MySQL database username
+$db_password = '  ';  // your MySQL database password
+$database = '  ';     // name of your MySQL database
+
+$db_connect = mysql_connect($db_host, $db_user, $db_password);
+$db = mysql_select_db($database,$db_connect);
