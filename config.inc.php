7abd791f0123964017eff30062367bb15e9764fe
Spencer Bravo <spencer@techandcode.com>
2014-09-18 22:40:34 -0700

    added php tags to clarify.

--- config.inc.php
+++ config.inc.php
@@ -1,3 +1,4 @@
+<?php 
 $db_host = '  ';      // location of your database(localhost ussualy works)
 $db_user = '  ';      // your MySQL database username
 $db_password = '  ';  // your MySQL database password
@@ -5,3 +6,4 @@ $database = '  ';     // name of your MySQL database
 
 $db_connect = mysql_connect($db_host, $db_user, $db_password);
 $db = mysql_select_db($database,$db_connect);
+?>
